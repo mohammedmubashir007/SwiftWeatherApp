6edaefc4d63644852c8be5d5d042e0fdd15863da
Mohammed Mubashir <mohammedmubashir204@gmail.com>
2025-04-19 18:54:18 +0530

    Initial Commit

---
+++ SwiftWeatherApp/SwiftWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftWeatherAppApp.swift
+//  SwiftWeatherApp
+//
+//  Created by Mohammed Mubashir on 19/04/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
